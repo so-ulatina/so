e4cae4ffb870f8d65ea911c12d3dd505c7c3c4e1
so-ulatina <59149022+so-ulatina@users.noreply.github.com>
2021-06-26 12:56:11 -0600

    Para los script
    Están con comentarios

---
+++ Practica/clase script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# ver variables de entorno
+# echo "El valor de la variable HOME es: "
+# echo $HOME
+
+# Usando un comando
+# echo "La salida del comando pwd es: "
+# pwd
+
+# Tome la salida y hágala legible
+# echo "El valor del comando pwd es $(pwd)"
+
+# Asignar salida de comando a una variable
+# output=$(pwd)
+# echo "El valor de la variable de salida es ${output}"
+
+# Ver datos en la línea de comando
+# echo "Ver $@ en la linea de comando"
+
+# Leer datos del usuario
+# echo "Ingrese un valor: "
+# read userInput
+# echo "Acabas de ingresar $userInput"
+
+# concatenar la entrada del usuario con la salida del comando
+# echo "Ingrese la extensión del archivo: "
+# read ext
+# touch "tuarchivo.${ext}"
+
+# compruebe si existe un archivo
+# if [ -d /etc/sysconfig ]; then
+#        echo "Ese archivo está ahí y un directorio"
+# else
+#        echo "No esta o no hay un directorio"
+# fi
